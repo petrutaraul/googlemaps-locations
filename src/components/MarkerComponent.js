65d1d2d3cb160f232ea0caaa761b34b9764a11f3
Raul Petruta <petruta.raul98@gmail.com>
2024-02-24 21:14:38 +0200

    Add dynamic icons based on location name

--- src/components/MarkerComponent.js
+++ src/components/MarkerComponent.js
@@ -10,9 +10,33 @@ const customIcon = L.icon({
   popupAnchor: [-3, -76],
 });
 
+const houseIcon = L.icon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+  // other properties...
+});
+
+const roadIcon = L.icon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+  // other properties...
+});
+
+const defaultIcon = L.icon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+  // other properties...
+});
+
 export default function MarkerComponent({ location }) {
+  let icon;
+  if (location.name.toLowerCase().includes("house")) {
+    icon = houseIcon;
+  } else if (location.name.toLowerCase().includes("road")) {
+    icon = roadIcon;
+  } else {
+    icon = defaultIcon;
+  }
+
   return (
-    <Marker position={location.latlng} icon={customIcon}>
+    <Marker position={location.latlng} icon={icon}>
       <Popup>
         <h2>Name: {location.name}</h2>
         <p>City: {location.city}</p>
